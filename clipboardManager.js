3769b2ceef002c17b0e6f84ce6ba0961573e506c
esturods <vincentroyce08@gmail.com>
2024-08-14 19:15:29 +0800

    Add clipboardManager
    Improved handling data

---
+++ clipboardManager.js
@@ -0,0 +1,249 @@
+const { clipboard, dialog } = require('electron');
+const { supabase } = require('./supabase.js');
+
+class ClipboardManager {
+  constructor() {
+    this.lastClipboardContent = '';
+    this.lastClipboardTextId = null;
+  }
+
+  // Method to upload data
+  async uploadData(userId, win, currentClipboardContent) {
+    try {
+      const { data, error } = await supabase
+        .from('clipboard')
+        .insert({
+          content: currentClipboardContent,
+          user_id: userId,
+        })
+        .select();
+
+      if (error) throw error;
+
+      this.lastClipboardTextId = data[0].id;
+      await this.deleteMatchingItems(userId, win, this.lastClipboardTextId);
+      await this.updateData('upload', win)
+
+      console.log('Clipboard content added:', currentClipboardContent);
+    } catch (error) {
+      console.error('Error adding clipboard content:', error.message);
+    }
+  }
+
+  // Method to check for clipboard changes
+  async checkClipboardChange(userId, win) {
+    const currentClipboardContent = clipboard.readText();
+
+    if (currentClipboardContent && currentClipboardContent !== this.lastClipboardContent && userId) {
+      this.lastClipboardContent = currentClipboardContent;
+
+      if (this.isPassword(currentClipboardContent)) {
+        const result = await dialog.showMessageBox({
+          type: 'error',
+          buttons: ['Yes', 'No'],
+          title: 'Possible Password Detected',
+          message: 'The copied text appears to be a password. Do you want to save it?',
+        });
+
+        if (result.response === 0) {
+          dialog.showMessageBox({
+            type: 'info',
+            title: 'Saving...',
+            message: 'No worries! Your password will be encrypted before storing it.',
+          });
+          await this.uploadData(userId, win, currentClipboardContent);
+        }
+      } else {
+        await this.uploadData(userId, win, currentClipboardContent);
+      }
+    }
+  }
+
+  // Method for deleting matching items
+  async deleteMatchingItems(userId, win, lastClipboardTextId) {
+    const currentClipboardContent = clipboard.readText();
+
+    try {
+      let { data: items, error } = await supabase
+        .from('decrypted_clipboard')
+        .select('*')
+        .eq('user_id', userId);
+
+      if (error) throw error;
+
+      const matchingItems = items
+        .filter(item => item.content === currentClipboardContent)
+        .filter(item => item.id !== lastClipboardTextId);
+
+      const itemIds = matchingItems.map(item => item.id);
+
+      if (itemIds.length > 0) {
+        const { error: deleteError } = await supabase
+          .from('clipboard')
+          .delete()
+          .in('id', itemIds)
+          .eq('user_id', userId);
+
+        if (deleteError) throw deleteError;
+
+        console.log('Deleted matching items:', itemIds);
+        await this.updateData('upload', win);
+      } else {
+        console.log('No matching items found.');
+      }
+    } catch (error) {
+      console.error('Error deleting matching items:', error.message);
+    }
+  }
+
+  // Method to fetch clipboard data
+  async fetch(userId, substring) {
+    try {
+      const { data, error } = await supabase
+        .from('decrypted_clipboard')
+        .select('*')
+        .eq('user_id', userId)
+        .ilike('content', `%${substring}%`)
+        .order('created_at', { ascending: false });
+
+      if (error) throw error;
+
+      return data;
+    } catch (error) {
+      console.error('Error fetching clipboard data:', error.message);
+      throw error;
+    }
+  }
+
+
+
+
+
+  // Delete Section
+
+  async deleteItem(userId, win, itemId) {
+    try {
+      const result = await dialog.showMessageBox({
+        type: 'question',
+        buttons: ['Yes', 'No'],
+        title: 'Confirm Deletion',
+        message: 'Do you want to delete this item?',
+      });
+
+      if (result.response === 0) { // Yes
+        const { error: deleteError } = await supabase
+          .from('clipboard')
+          .delete()
+          .eq('id', itemId)
+          .eq('user_id', userId);
+
+        if (deleteError) throw deleteError;
+
+        console.log(`Item ${itemId} deleted from database`);
+        await this.updateData('delete', win);
+      }
+    } catch (error) {
+      console.error('Error deleting item:', error.message);
+    }
+  }
+
+  async deleteBrokenItem(userId, win, itemId) {
+    try {
+      await dialog.showMessageBox({
+        type: 'info',
+        title: 'Broken Link Deleted',
+        message: 'A copied broken link was automatically deleted.',
+      });
+
+      const { error: deleteError } = await supabase
+        .from('clipboard')
+        .delete()
+        .eq('id', itemId)
+        .eq('user_id', userId);
+
+      if (deleteError) throw deleteError;
+
+      console.log(`Item ${itemId} deleted from database`);
+      await this.updateData('delete', win);
+    } catch (error) {
+      console.error('Error deleting broken item:', error.message);
+    }
+  }
+
+  async deleteOldItems(userId, win) {
+    const oneWeekAgo = new Date();
+    oneWeekAgo.setHours(oneWeekAgo.getHours() - 168);
+    const oneMonthAgo = new Date();
+    oneMonthAgo.setHours(oneMonthAgo.getHours() - 720);
+    const now = new Date();
+    const startOfToday = new Date(now);
+    startOfToday.setHours(0, 0, 0, 0); // Set to start of day (00:00:00)
+
+    try {
+      const result = await dialog.showMessageBox({
+        type: 'question',
+        buttons: ['From today', 'Older than 7 days', 'Older than 30 days', 'All', 'Cancel'],
+        title: 'Confirm Deletion',
+        message: 'Delete items...',
+      });
+
+      if (result.response === 1) {
+        const { data, error } = await supabase
+          .from('clipboard')
+          .delete()
+          .lt('created_at', oneWeekAgo.toISOString())
+          .eq('user_id', userId);
+
+        if (error) throw error;
+        console.log('Old items deleted from database:', 'older than 7 days');
+      } else if (result.response === 2) {
+        const { data, error } = await supabase
+          .from('clipboard')
+          .delete()
+          .lt('created_at', oneMonthAgo.toISOString())
+          .eq('user_id', userId);
+
+        if (error) throw error;
+        console.log('Old items deleted from database:', 'older than 30 days');
+      } else if (result.response === 0) {
+        const { data, error } = await supabase
+          .from('clipboard')
+          .delete()
+          .gte('created_at', startOfToday.toISOString())
+          .lte('created_at', now.toISOString());
+
+        if (error) throw error;
+        console.log('Old items deleted from database:', 'from today');
+      } else if (result.response === 3) {
+        const { data, error } = await supabase
+          .from('clipboard')
+          .delete()
+          .eq('user_id', userId);
+
+        if (error) throw error;
+        console.log('Old items deleted from database:', 'all');
+      }
+
+      await this.updateData('delete', win);
+    } catch (error) {
+      console.error('Error deleting old items:', error.message);
+    }
+  }
+
+
+  // Method to check if content is a password
+  isPassword(text) {
+    const passwordPatterns = [/password/i]; // Add more patterns if needed
+    return passwordPatterns.some(pattern => pattern.test(text));
+  }
+
+  updateData(action, win) {
+    win.webContents.send('update-data', action);
+  }
+
+}
+
+
+  
+
+module.exports = ClipboardManager;
--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const { app, BrowserWindow, ipcMain, clipboard, screen, dialog} = require('electron');
 const path = require('path');
 const { autoUpdater } = require('electron-updater');
-const { supabase, SECRET_KEY } = require('./supabase.js');
+const { supabase } = require('./supabase.js');
+const ClipboardManager = require('./clipboardManager.js');
 
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
@@ -11,22 +12,23 @@ const crypto = require('crypto');
 const ogs = require('open-graph-scraper');
 const log = require('electron-log');
 
+const clipboardManager = new ClipboardManager();
+
+let store;
+let win;
+let userId;
+
+// let lastImageHash = '';
+
+let screenWidth;
+let screenHeight;
+
+
 app.commandLine.appendSwitch('enable-transparent-visuals');
 app.commandLine.appendSwitch('disable-gpu');
 log.transports.file.level = 'info';
 autoUpdater.logger = log;
 log.info('App starting...');
-
-let store;
-let win;
-
-let userId;
-let lastClipboardContent = '';
-let lastImageHash = '';
-
-
-let screenWidth;
-let screenHeight;
 
 function createWindow() {
   const primaryDisplay = screen.getPrimaryDisplay();
@@ -149,15 +151,6 @@ ipcMain.handle('reset-password', async (event, { email }) => {
     }
 });
 
-
-ipcMain.handle('upload-clipboard-data', async() => {
-    // handleClipboardImage()
-    checkClipboardChange()
-   
-  
-})
-
-
   
 ipcMain.on('minimize-window', () => {
   win.minimize();
@@ -187,139 +180,35 @@ const { width: currentWidth, height: currentHeight } = win.getBounds();
 
   
 ipcMain.handle('fetch-clipboard-data', async () => {
-  
-   
-    try {
-      const { data, error } = await supabase
-        .from('decrypted_clipboard')
-        .select('*')
-        .eq('user_id', userId)
-        .order('created_at', { ascending: false });
+  const substring = ''
+  try {
+    const data = await clipboardManager.fetch(userId, substring)
+  return data
+  } catch (error) {
+    console.error('Error fetching clipboard data:', error.message);
 
+  }
   
-      if (error) throw error;
-      // console.log(data)
-  
-  
-      return data;
-    } catch (error) {
-      console.error('Error fetching clipboard data:', error.message);
-      throw error;
-    }
 });
 
-  
-  
+ipcMain.handle('upload-clipboard-data', async() => {
+  // handleClipboardImage()
+  await clipboardManager.checkClipboardChange(userId);
+})
+
+
 ipcMain.on('delete-item', async (event, itemId) => {
-      try {
-  
-        const result = await dialog.showMessageBox({
-          type: 'question',
-          buttons: ['Yes', 'No'],
-          title: 'Confirm Deletion',
-          message: `Do you want to delete this item?`,
-        });
-  
-        if (result.response === 0) { // User clicked 'Yes'
-        const { error: deleteError } = await supabase
-          .from('clipboard')
-          .delete()
-          .eq('id', itemId)
-          .eq('user_id', userId);
-  
-        if (deleteError) throw deleteError;
-        console.log(`Item ${itemId} deleted from database`);
-        updateData('delete');
-        }
-      } catch (error) {
-        console.error('Error deleting item:', error.message);
-      }
+  await clipboardManager.deleteItem(userId, win, itemId);
 });
+
   
 ipcMain.on('delete-broken-item', async (event, itemId) => {
-      try {
-        dialog.showMessageBox({
-          type: 'info',
-          title: 'Broken Link Deleted',
-          message: `A copied broken link was automatically deleted.`,
-        });
-        const { error: deleteError } = await supabase
-          .from('clipboard')
-          .delete()
-          .eq('id', itemId)
-          .eq('user_id', userId);
-  
-        if (deleteError) throw deleteError;
-        console.log(`Item ${itemId} deleted from database`);
-        updateData('delete');
-
-        } catch (error) {
-        console.error('Error deleting item:', error.message);
-      }
+  await clipboardManager.deleteBrokenItem(userId, win, itemId);
 });
-  
+
+
 ipcMain.on('delete-old-items', async (event) => {
-      const oneWeekAgo = new Date();
-      oneWeekAgo.setHours(oneWeekAgo.getHours() - 168);
-      const oneMonthAgo = new Date();
-      oneMonthAgo.setHours(oneMonthAgo.getHours() - 720);
-      const now = new Date()
-      const startOfToday = new Date(now);
-      startOfToday.setHours(0, 0, 0, 0); // Set to start of day (00:00:00)
-      
-      try {
-  
-        const result = await dialog.showMessageBox({
-          type: 'question',
-          buttons: [' From today', ' older than 7 days', 'older than 30 days', 'All', 'Cancel'],
-          title: 'Confirm Deletion',
-          message: `Delete items...`,
-        });
-
-        if (result.response === 1) {
-        const { data, error } = await supabase
-          .from('clipboard')
-          .delete()
-          .lt('created_at', oneWeekAgo.toISOString())
-          .eq('user_id', userId); 
-        if (error) throw error;
-        console.log('Old items deleted from database:', 'older than 7 days');
-        } 
-        
-        else if (result.response === 2) {
-          const { data, error } = await supabase
-          .from('clipboard')
-          .delete()
-          .lt('created_at', oneMonthAgo.toISOString())
-          .eq('user_id', userId);
-        if (error) throw error;
-        console.log('Old items deleted from database:', 'older than 30 days');
-        } 
-        
-        else if (result.response === 0) {
-          const { data, error } = await supabase
-          .from('clipboard')
-          .delete()
-          .gte('created_at', startOfToday.toISOString())
-          .lte('created_at', now.toISOString());
-        if (error) throw error;
-        console.log('Old items deleted from database:', 'from today');
-        } 
-        
-        else if (result.response === 3) {
-          const { data, error } = await supabase
-          .from('clipboard')
-            .delete()
-            .eq('user_id', userId);
-        if (error) throw error;
-        console.log('Old items deleted from database:', 'all');
-        }
-        updateData('delete');
-
-        
-      } catch (error) {
-        console.error('Error deleting old items:', error.message);
-      }
+  await clipboardManager.deleteOldItems(userId, win);
 
 });
 
@@ -341,12 +230,20 @@ supabase.auth.onAuthStateChange((event, session) => {
 });
 
 
+
+
+
+
+
+
 app.on('ready', async () => {
   createWindow();
   const ElectronStore = (await import('electron-store')).default;
   store = new ElectronStore();
   await autoLogin();
-  setInterval(checkClipboardChange, 1000); //
+  setInterval(() => {
+    clipboardManager.checkClipboardChange(userId, win);
+  }, 1000);
 
 
 });
@@ -359,6 +256,9 @@ app.on('activate', () => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
+
+
 
 
 async function autoLogin() {
@@ -392,195 +292,84 @@ async function autoLogin() {
   
 
   
-async function checkClipboardChange() {
-  const currentClipboardContent = clipboard.readText();
   
-  if (currentClipboardContent && currentClipboardContent !== lastClipboardContent && userId) {
-    lastClipboardContent = currentClipboardContent;
-    if (isPassword(currentClipboardContent)) {
-  
-      const result = await dialog.showMessageBox({
-        type: 'error',
-        buttons: ['Yes', 'No'],
-        title: 'Possible Password Detected',
-        message: 'The copied text appears to be a password. Do you want to save it?',
-      })
-        if (result.response === 0) { 
-          dialog.showMessageBox({
-            type: 'info',
-            title: 'Saving...',
-            message: 'No worries! Your password will be encrypted before storing it.',
-          });
-          uploadData(currentClipboardContent)
-          } else {
-
-          } 
-
-        } else {
-          uploadData(currentClipboardContent)
-
-
-
-        }
-      
-    }
-  
-}
-  
-async function uploadData(currentClipboardContent) {
-          
-    try {
-      const { data, error } = await supabase
-        .from('clipboard')
-        .insert({ 
-          content: currentClipboardContent,
-          user_id: userId
-         })
-        .select();
-        // console.log(data)
-  
-         lastClipboardTextId = data[0].id;
-         deleteMatchingItems(lastClipboardTextId)
-
+//  Excluded Features:
 
   
-  
-      if (error) throw error;
-      console.log('Clipboard content added:', currentClipboardContent);
-
-    } catch (error) {
-      console.error('Error adding clipboard content:', error.message);
-    }
-}
-  
-async function handleClipboardImage() {
-      const currentClipboardImage = clipboard.readImage();
+// async function handleClipboardImage() {
+//       const currentClipboardImage = clipboard.readImage();
     
-      // console.log(currentClipboardImage)
+//       // console.log(currentClipboardImage)
       
-        if (!currentClipboardImage.isEmpty()) {
-          const imageBuffer = currentClipboardImage.toPNG();
-          const imageId = uuidv4();
-          const imagePath = path.join(__dirname, `images/${imageId}.png`);
-          const folderPath = path.join(__dirname, 'images');
-          await promisify(fs.writeFile)(imagePath, imageBuffer);
-          console.log('Image saved:', imagePath);
+//         if (!currentClipboardImage.isEmpty()) {
+//           const imageBuffer = currentClipboardImage.toPNG();
+//           const imageId = uuidv4();
+//           const imagePath = path.join(__dirname, `images/${imageId}.png`);
+//           const folderPath = path.join(__dirname, 'images');
+//           await promisify(fs.writeFile)(imagePath, imageBuffer);
+//           console.log('Image saved:', imagePath);
       
-          const base64ImageData = imageBuffer.toString('base64');
-          const currentImageHash = crypto.createHash('sha256').update(base64ImageData).digest('hex');
+//           const base64ImageData = imageBuffer.toString('base64');
+//           const currentImageHash = crypto.createHash('sha256').update(base64ImageData).digest('hex');
       
-          if (currentImageHash !== lastImageHash) {
-            lastImageHash = currentImageHash;
+//           if (currentImageHash !== lastImageHash) {
+//             lastImageHash = currentImageHash;
       
-            try {
-              const arrayBuffer = decode(base64ImageData);
-              const { data, error } = await supabase
-                .storage
-                .from('images')
-                .upload(`${imageId}.png`, arrayBuffer, {
-                  contentType: 'image/png',
-                  user_id: userId
-                });
+//             try {
+//               const arrayBuffer = decode(base64ImageData);
+//               const { data, error } = await supabase
+//                 .storage
+//                 .from('images')
+//                 .upload(`${imageId}.png`, arrayBuffer, {
+//                   contentType: 'image/png',
+//                   user_id: userId
+//                 });
       
-              if (error) throw error;
+//               if (error) throw error;
       
-              await supabase.from('clipboard').insert({
-                image_url: `${SUPABASE_URL}/storage/v1/object/public/images/${imageId}.png`,
-              });
-              console.log('Image URL added to database:', `${SUPABASE_URL}/storage/v1/object/public/images/${imageId}.png`);
+//               await supabase.from('clipboard').insert({
+//                 image_url: `${SUPABASE_URL}/storage/v1/object/public/images/${imageId}.png`,
+//               });
+//               console.log('Image URL added to database:', `${SUPABASE_URL}/storage/v1/object/public/images/${imageId}.png`);
       
             
-              deleteFolderContent(folderPath);
-            } catch (error) {
-              console.error('Error handling clipboard image:', error.message);
-            }
-          } else {
-            console.log('No change detected in clipboard image.');
-            const filePath = path.join(__dirname, `images/${imageId}.png`);
-            // fs.unlink(filePath, err => {
-            //   if (err) {
-            //     console.error(`Error deleting file ${filePath}:`, err.message);
-            //   } else {
-            //     console.log(`Successfully deleted file ${filePath}`);
-            //   }})
-          }
-        }
-}
+//               deleteFolderContent(folderPath);
+//             } catch (error) {
+//               console.error('Error handling clipboard image:', error.message);
+//             }
+//           } else {
+//             console.log('No change detected in clipboard image.');
+//             const filePath = path.join(__dirname, `images/${imageId}.png`);
+//             // fs.unlink(filePath, err => {
+//             //   if (err) {
+//             //     console.error(`Error deleting file ${filePath}:`, err.message);
+//             //   } else {
+//             //     console.log(`Successfully deleted file ${filePath}`);
+//             //   }})
+//           }
+//         }
+// }
     
-
-async function deleteMatchingItems(lastClipboardTextId) {
-      const currentClipboardContent = clipboard.readText();
-  
-    try {
-      let { data: items, error } = await supabase
-        .from('decrypted_clipboard')
-        .select('*')
-        .eq('user_id', userId);
-  
-      if (error) throw error;
-  
-      const matchingItems = items
-        .filter(item => item.content === currentClipboardContent)
-        .filter(item => item.id !== lastClipboardTextId);
-  
-      const itemIds = matchingItems.map(item => item.id);
-  
-      if (itemIds.length > 0) {
-        const { error: deleteError } = await supabase
-          .from('clipboard')
-          .delete()
-          .in('id', itemIds)
-          .eq('user_id', userId);
-  
-        if (deleteError) throw deleteError;
-  
-        console.log('Deleted matching items:', itemIds);
-        updateData('upload');
-
-        
-      } else {
-        console.log('No matching items found.');
-      }
-    } catch (error) {
-      console.error('Error deleting matching items:', error.message);
-    }
-}
  
-function deleteFolderContent(folderPath) {
-      fs.readdir(folderPath, (err, files) => {
-        if (err) {
-          console.error(`Error reading folder ${folderPath}:`, err.message);
-          return;
-        }
+// function deleteFolderContent(folderPath) {
+//       fs.readdir(folderPath, (err, files) => {
+//         if (err) {
+//           console.error(`Error reading folder ${folderPath}:`, err.message);
+//           return;
+//         }
     
-        files.forEach(file => {
-          const filePath = path.join(folderPath, file);
-          fs.unlink(filePath, err => {
+//         files.forEach(file => {
+//           const filePath = path.join(folderPath, file);
+//           fs.unlink(filePath, err => {
   
-            if (err) {
-              console.error(`Error deleting file ${filePath}:`, err.message);
-            } else {
-              console.log(`Successfully deleted file ${filePath}`);
-            }
-          });
-        });
-      });
-}
-  
-  
-function isPassword(text) {
-      // const lengthCriteria = text.length >= 8;
-      // const varietyCriteria = /[A-Z]/.test(text) && /[a-z]/.test(text) && /[0-9]/.test(text) && /[!@#$%^&*()_+]/.test(text);
+//             if (err) {
+//               console.error(`Error deleting file ${filePath}:`, err.message);
+//             } else {
+//               console.log(`Successfully deleted file ${filePath}`);
+//             }
+//           });
+//         });
+//       });
+// }
   
   
-      const passwordPatterns = [
-        /password/i, 
-      
-      ];
-      return passwordPatterns.some(pattern => pattern.test(text)) 
-
-}
-
-function updateData(action) {
-  win.webContents.send('update-data', action);
-}
